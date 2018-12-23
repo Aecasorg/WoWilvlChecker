49e96546d48273d6855a1f782edeb8b83f6ed214
Henrik Gustavii <henrik@gustavii.com>
2018-12-23 11:39:40 +0100

    Missed one realm reference...

--- WoWilvlChecker/AppDelegate.swift
+++ WoWilvlChecker/AppDelegate.swift
@@ -7,25 +7,13 @@
 //
 
 import UIKit
-import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-//    let realm = try! Realm()
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-//        print(Realm.Configuration.defaultConfiguration.fileURL)
-        
-        do {
-            _ = try Realm()
-        } catch {
-            print("Error initialising new realm \(error)")
-        }
-        
         return true
     }
 
